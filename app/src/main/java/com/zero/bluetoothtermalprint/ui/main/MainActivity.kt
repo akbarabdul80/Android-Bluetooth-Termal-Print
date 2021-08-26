efdfeea9adf24c7c86c47475dde57e6da63a8096
akbarabdul80 <akbarabdul80@gmail.com>
2021-08-26 23:25:21 +0700

    Fix gradle,  add fragment_product.xml

--- app/src/main/java/com/zero/bluetoothtermalprint/ui/main/MainActivity.kt
+++ app/src/main/java/com/zero/bluetoothtermalprint/ui/main/MainActivity.kt
@@ -2,9 +2,13 @@ package com.zero.bluetoothtermalprint.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.oratakashi.viewbinding.core.binding.activity.viewBinding
+import com.oratakashi.viewbinding.core.tools.onClick
 import com.zero.bluetoothtermalprint.R
 import com.zero.bluetoothtermalprint.data.model.product.DataProduct
+import com.zero.bluetoothtermalprint.databinding.ActivityMainBinding
 import com.zero.bluetoothtermalprint.root.App
+import com.zero.bluetoothtermalprint.ui.product.ProductFragment
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
@@ -13,25 +17,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch {
-            App.repoProduct.insertProduct(
-                arrayListOf(
-                    DataProduct(
-                        0,
-                        "Keyboard Gaming",
-                        "ready stock red, yellow",
-                        500000,
-                        12
-                    ),
-                    DataProduct(
-                        0,
-                        "Keyboard Gaming",
-                        "Kready stock red, yellow ",
-                        500000,
-                        12
+        with(binding) {
+            GlobalScope.launch {
+                App.repoProduct.insertProduct(
+                    arrayListOf(
+                        DataProduct(
+                            0,
+                            "Keyboard Gaming",
+                            "ready stock red, yellow",
+                            500000,
+                            12
+                        ),
+                        DataProduct(
+                            0,
+                            "Keyboard Gaming",
+                            "Kready stock red, yellow ",
+                            500000,
+                            12
+                        )
                     )
                 )
-            )
+            }
+
+            fabProduct.onClick {
+                ProductFragment().show(supportFragmentManager, "Product")
+            }
         }
+
     }
+
+    private val binding: ActivityMainBinding by viewBinding()
 }
---
+++ app/src/main/java/com/zero/bluetoothtermalprint/ui/product/ProductFragment.kt
@@ -0,0 +1,20 @@
+package com.zero.bluetoothtermalprint.ui.product
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.zero.bluetoothtermalprint.R
+
+
+class ProductFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_product, container, false)
+    }
+}
